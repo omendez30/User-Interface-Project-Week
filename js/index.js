7c6b2933033388eb4f0749095a32be324b1414df
omendez30 <omendez9@outlook.com>
2019-02-19 22:59:19 -0500

    Navigation funcitonality for each page

--- js/index.js
+++ js/index.js
@@ -24,47 +24,12 @@ let expand = document
   .querySelectorAll(".main-nav")
   .forEach(element => new Expanded(element));
 
-// class TabLink {
-//   constructor(tab) {
-//     this.tab = tab;
-//     //console.log(this.tab);
-//     this.data = this.tab.dataset.tab;
-//     //console.log(this.data);
-//     this.itemElement = document.querySelector(
-//       `.tabs-link[data-tab ="${this.data}"]`
-//     );
-
-//     this.tabItem = new TabItem(this.itemElement);
-//     this.tab.addEventListener("click", () => this.activeTab());
-//   }
-//   activeTab() {
-//     const link = document.querySelectorAll(".tabs-link");
-
-//     link.forEach(link => link.classList.remove("tabs-links-selected"));
-
-//     this.tab.classList.add("tab-link-selected");
-
-//     this.tabItem.activeTab();
-//   }
-// }
-
-// class TabItem {
-//   constructor(tab) {
-//     this.tab = tab;
-//   }
-//   activeTab() {
-//     const items = document.querySelectorAll(".tab-item");
-
-//     items.forEach(item => item.classList.remove("tab-item-selected"));
-
-//     this.tab.classList.add("tab-item-selected");
-
-//     this.tab.style.backgroundColor = "teal";
-
-//     //this.tab.activeTab();
-//   }
-// }
-// let links = document
-//   .querySelectorAll(".tabs .tabs-link")
-//   .forEach(tab => new TabLink(tab));
-// //console.log(links);
+class Tabs {
+  constructor(element) {
+    this.element = element;
+  }
+}
+let links = document
+  .querySelectorAll(".tabs .tabs-link")
+  .forEach(tab => new Tabs(tab));
+//console.log(links);
